94791611a9607ea5e274530a259ee03760519c8f
vitokitoonline <shiroshana@gmail.com>
2013-12-22 14:08:52 +0000

    acid data 002

--- linear_regression_acidityData.py
+++ linear_regression_acidityData.py
@@ -3,36 +3,24 @@ This code is to analyse linear Regression and Gradient Decent
 Original author https://gist.github.com/marcelcaraciolo/1321575 
 Modified by https://github.com/ickyatcity
 ***********************************************************'''
-
 import numpy as np 
 import pylab as pl
 import csv
-
 from pylab import *
 from numpy import *
 
-
 def compute_cost(x, y, theta):
-
-#number of training examples
-	m = y.size
-
-	predictions = x.dot(theta).flatten()
-
-	sqError = (predictions - y.astype(np.float))**2
-
-	J = (1.0/(2.0 * m)) *sqError.sum()
-
-	return J
-
-
+    #number of training examples
+    m = y.size
+    predictions = x.dot(theta).flatten()
+    sqError = (predictions - y.astype(np.float))**2
+    J = (1.0/(2.0 * m)) *sqError.sum()
+    return J
 
 def gradient_descent(x, y, theta, alpha, iterations):
-
-## numer of training samples
+    ## numer of training samples
     m = y.size
     J_history = zeros(shape = (iterations, 1))
-
     for i in xrange(iterations):
         predictions = x.dot(theta).flatten()
         error_x0 = (predictions - y.astype(np.float))*x[:,0]
@@ -40,24 +28,21 @@ def gradient_descent(x, y, theta, alpha, iterations):
 	theta[0][0] = theta[0][0] - (alpha*(1.0 / m) * error_x0.sum())
 	theta[1][0] = theta[1][0] - alpha*(1.0 / m) * error_x1.sum()
     	J_history[i, 0] = compute_cost(x, y, theta)
-
     return theta, J_history
-
-
 
 ''' **************************************************
 begin main code 
 ******************************************************
 '''
-dataset = [ ]
+dataset = [ ] 
+#read dataset found here: http://archive.ics.uci.edu/ml/datasets/Wine+Quality
 with open ('dataset.csv', 'rb') as csvfile:
     csv_read = csv.reader(csvfile, delimiter = ";")
     header = csv_read.next()
     for row in csv_read:
         dataset.append(row)
-
-
 dataset = np.array(dataset)
+
 
 '''
 the dataset contains the following features. For the purpose of this we will choose fixed acidity and density
@@ -66,28 +51,24 @@ the dataset contains the following features. For the purpose of this we will cho
 "total sulfur dioxide";"density";"pH";
 "sulphates";"alcohol";"quality"
 '''
-
 #assign data to varaibles x and y
 x = dataset[:,8]
 y = dataset[:,0]
 
 ##plot data set uploaded to for viewing
+
 #pl.scatter(x, y)
 #pl.axis('tight')
 #pl.show()
 
-
 #declare varaibles
-
 #number of observations 
-m = y.size
 
+m = y.size
 # add a column to the x:
 # this creates a (n x 2) matrix
-
 it = ones(shape = (m, 2))
 it[:,1] = x
- 
 
 #declare theta
 #this creates a (2 x 1) matrix
@@ -95,7 +76,6 @@ it[:,1] = x
 # (n x 2) (2 x1) = (n x 1)
 
 theta = zeros(shape = (2,1))
-
 
 #some gradient descent settings
 # these numbers are trial and error to some extent.
@@ -109,18 +89,15 @@ alpha = 0.01
 
 theta, J_history = gradient_descent(it, y, theta, alpha, iterations)
 print 'value of theta', theta
-
  
 ##Plot the results
 result = it.dot(theta).flatten()
 pl.scatter (dataset[:,0], result, color = "red")
 pl.show()
  
- 
 #Grid over which we will calculate J
 theta0_vals = linspace(-10, 10, 100)
 theta1_vals = linspace(-1, 4, 100)
- 
  
 #initialize J_vals to a matrix of 0's
 J_vals = zeros(shape=(theta0_vals.size, theta1_vals.size))
@@ -132,8 +109,6 @@ for t1, element in enumerate(theta0_vals):
         thetaT[0][0] = element
         thetaT[1][0] = element2
         J_vals[t1, t2] = compute_cost(it, y, thetaT)
-
-
  
 #Contour plot
 J_vals = J_vals.T
